a62251d346a8455df0aacfbac1492041b471795f
Alexis Rollin <alexis.rollin@etu.univ-grenoble-alpes.fr>
2020-11-10 17:52:30 +0100

    Register page updated and register logic modified (WIP)

--- uwind/src/main/webapp/app/account/register/register.component.ts
+++ uwind/src/main/webapp/app/account/register/register.component.ts
@@ -6,6 +6,9 @@ import { JhiLanguageService } from 'ng-jhipster';
 import { EMAIL_ALREADY_USED_TYPE, LOGIN_ALREADY_USED_TYPE } from 'app/shared/constants/error.constants';
 import { LoginModalService } from 'app/core/login/login-modal.service';
 import { RegisterService } from './register.service';
+import { EtudiantService } from '../../entities/etudiant/etudiant.service';
+import { ProfilService } from 'app/entities/profil/profil.service';
+import { UserService } from 'app/core/user/user.service';
 
 @Component({
   selector: 'jhi-register',
@@ -34,12 +37,28 @@ export class RegisterComponent implements AfterViewInit {
     email: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(254), Validators.email]],
     password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(50)]],
     confirmPassword: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(50)]],
+
+    //Relative to a student
+    niveauScolaire: [null, [Validators.required]],
+    departement: [null, [Validators.required]],
+    niveauPlanche: [null, [Validators.required]],
+    permisDeConduire: [null, [Validators.required]],
+    lieuDepart: [null, [Validators.required]],
+    optionSemestre: [null, [Validators.required]],
+    profil: [],
+    flotteur: [],
+    voile: [],
+    combinaison: [],
+    gestionnaire: [],
   });
 
   constructor(
     private languageService: JhiLanguageService,
     private loginModalService: LoginModalService,
     private registerService: RegisterService,
+    private userService: UserService, //to retrieve lastly created user
+    private profilService: ProfilService, //to create new profile
+    private etudiantService: EtudiantService, //to create new student
     private fb: FormBuilder
   ) {}
 
@@ -61,10 +80,41 @@ export class RegisterComponent implements AfterViewInit {
     } else {
       const login = this.registerForm.get(['login'])!.value;
       const email = this.registerForm.get(['email'])!.value;
+
+      //creating a new profil
+      const prenom = this.registerForm.get(['prenom'])!.value;
+      const nom = this.registerForm.get(['nom'])!.value;
+      const numTel = this.registerForm.get(['numTel'])!.value;
+
+      //creating an Etudiant entity after inscription
+      const niveauScolaire = this.registerForm.get(['niveauScolaire'])!.value;
+      const departement = this.registerForm.get(['departement'])!.value;
+      const niveauPlanche = this.registerForm.get(['niveauPlanche'])!.value;
+      const permisDeConduire = this.registerForm.get(['permisDeConduire'])!.value;
+      const lieuDepart = this.registerForm.get(['lieuDepart'])!.value;
+      const optionSemestre = this.registerForm.get(['optionSemestre'])!.value;
+
+      //Saving user
       this.registerService.save({ login, email, password, langKey: this.languageService.getCurrentLanguage() }).subscribe(
         () => (this.success = true),
         response => this.processError(response)
       );
+
+      /*
+      //Pushing profil to DB
+      this.userService.find(login).subscribe( response => )
+
+      this.ProfilService.create ( {id, prenom, nom, email, numTel, utilisateur).subscribe(
+        () => (this.success = true),
+        response => this.processError(response)
+      );
+      
+      //Pushing student to DB
+      this.etudiantService.create({ , niveauScolaire, departement, niveauPlanche,
+      permisDeConduire, lieuDepart, optionSemestre, false, profil, [], [], [], [] }).subscribe(
+        () => (this.success = true),
+        response => this.processError(response)
+      );*/
     }
   }
 
