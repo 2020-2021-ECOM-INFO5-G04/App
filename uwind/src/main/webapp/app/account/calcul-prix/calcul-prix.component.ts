52f5e43414983018c1159d96f85955eb5fb63c9d
Pareilleux Killian <killian.pareilleux@etu.unilim.fr>
2020-11-30 21:39:54 +0100

    Added price page, and created router

---
+++ uwind/src/main/webapp/app/account/calcul-prix/calcul-prix.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jhi-calcul-prix',
+  templateUrl: './calcul-prix.component.html',
+})
+export class CalculPrixComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
