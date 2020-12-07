aa9bdaef87b02174932da245529918ad8e973ac7
Pareilleux Killian <killian.pareilleux@etu.unilim.fr>
2020-12-07 14:40:13 +0100

    First_Test_Routing_WIP

--- uwind/src/main/webapp/app/account/calcul-prix/calcul-prix.component.ts
+++ uwind/src/main/webapp/app/account/calcul-prix/calcul-prix.component.ts
@@ -5,7 +5,15 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './calcul-prix.component.html',
 })
 export class CalculPrixComponent implements OnInit {
+  error = false;
+  success = false;
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  pay(): void {
+    this.error = false;
+    // add payment service and code
+  }
 }
