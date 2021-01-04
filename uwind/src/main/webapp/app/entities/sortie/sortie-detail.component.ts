8839fac4e6a6e3d521dee73698e8b1b0f27012f5
nhatquang-ho <Nhat-Quang.Ho@etu.univ-grenoble-alpes.fr>
2021-01-04 15:43:36 +0100

    add map

--- uwind/src/main/webapp/app/entities/sortie/sortie-detail.component.ts
+++ uwind/src/main/webapp/app/entities/sortie/sortie-detail.component.ts
@@ -3,12 +3,29 @@ import { ActivatedRoute } from '@angular/router';
 
 import { ISortie } from 'app/shared/model/sortie.model';
 
+import * as L from 'leaflet';
+
 @Component({
   selector: 'jhi-sortie-detail',
   templateUrl: './sortie-detail.component.html',
 })
 export class SortieDetailComponent implements OnInit {
   sortie: ISortie | null = null;
+
+  options1 = {
+    layers: [L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })],
+    zoom: 15,
+    center: L.latLng(45.1933502, 5.7647807),
+  };
+  layers1 = [
+    L.marker([45.1933502, 5.7647807], {
+      icon: L.icon({
+        iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+        iconUrl: require('leaflet/dist/images/marker-icon.png'),
+        shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+      }),
+    }),
+  ];
 
   constructor(protected activatedRoute: ActivatedRoute) {}
 
@@ -18,5 +35,10 @@ export class SortieDetailComponent implements OnInit {
 
   previousState(): void {
     window.history.back();
+  }
+  onMapReady(map: L.Map): void {
+    setTimeout(() => {
+      map.invalidateSize(true);
+    }, 100);
   }
 }
