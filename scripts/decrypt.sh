af4ad0a23a9b20fb994e7d23456934025e7d9f19
Alexis Rollin <alexis.rollin@etu.univ-grenoble-alpes.fr>
2021-02-05 11:50:40 +0100

    Fix file path error

--- scripts/decrypt.sh
+++ scripts/decrypt.sh
@@ -3,4 +3,4 @@
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASS" \
---output ../testfile2.json ../testfile.json.gpg
+--output ../uwind/testfile2.json ../uwind/testfile.json.gpg
