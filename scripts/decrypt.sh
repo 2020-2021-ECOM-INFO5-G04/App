b9852225fe2fced61daf8134dbe95c7737e9c87d
Alexis Rollin <alexis.rollin@etu.univ-grenoble-alpes.fr>
2021-02-05 14:27:26 +0100

    Filepath problem understanding

--- scripts/decrypt.sh
+++ scripts/decrypt.sh
@@ -2,5 +2,6 @@
 
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
+cat $HOME/uwind/testfile.json
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASS" \
 --output $HOME/uwind/testfile2.json $HOME/uwind/testfile.json.gpg
